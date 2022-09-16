ce8afc15c8807da507c1ec1906218e2d71bcab48
chmery <karektv@gmail.com>
2022-09-16 13:43:41 +0200

    Sign up functionality & inputs validation

--- components/Auth/AuthForm.tsx
+++ components/Auth/AuthForm.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import styles from "./AuthForm.module.css";
+import Spinner from "../UI/Spinner/Spinner";
 
 type Props = {
-    onAuth: () => void;
+    onAuth: (email: string, password: string) => void;
     action: string;
+    isLoading: boolean;
 };
 
-const AuthForm = ({ onAuth, action }: Props) => {
+const AuthForm = ({ onAuth, action, isLoading }: Props) => {
     const [isEmailValid, setIsEmailValid] = useState(true);
     const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
 
     const enteredEmail = useRef<HTMLInputElement>(null!);
     const enteredPassword = useRef<HTMLInputElement>(null!);
@@ -32,21 +36,32 @@ const AuthForm = ({ onAuth, action }: Props) => {
         return action === "login" ? LoginPageText : SignUpPageText;
     };
 
+    const areInputsValid = (email: string, password: string) => {
+        const emailRegex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+        const isEmailCorrect = emailRegex.test(email);
+
+        if (!isEmailCorrect) {
+            setIsEmailValid(false);
+            const error = "Enter correct e-mail address.";
+            setEmailError(error);
+        }
+
+        if (password.length < 6) {
+            const error = "Password should be at least 6 characters long.";
+            setIsPasswordValid(false);
+            setPasswordError(error);
+        }
+
+        return password.length >= 6 && isEmailCorrect ? true : false;
+    };
+
     const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const email = enteredEmail.current.value.trim();
         const password = enteredPassword.current.value.trim();
 
-        if (!email) {
-            setIsEmailValid(false);
-        }
-
-        if (!password) {
-            setIsPasswordValid(false);
-        }
-
-        if (isEmailValid && isPasswordValid) {
-            onAuth();
+        if (areInputsValid(email, password)) {
+            onAuth(email, password);
         }
     };
 
@@ -66,9 +81,7 @@ const AuthForm = ({ onAuth, action }: Props) => {
                     placeholder="E-mail"
                     onFocus={focusHandler}
                 />
-                {!isEmailValid && (
-                    <p className={styles["invalid-message"]}>E-mail field can't be empty.</p>
-                )}
+                {!isEmailValid && <p className={styles["invalid-message"]}>{emailError}</p>}
             </div>
 
             <div>
@@ -79,12 +92,18 @@ const AuthForm = ({ onAuth, action }: Props) => {
                     placeholder="Password"
                     onFocus={focusHandler}
                 />
-                {!isPasswordValid && (
-                    <p className={styles["invalid-message"]}>Password field can't be empty.</p>
-                )}
+                {!isPasswordValid && <p className={styles["invalid-message"]}>{passwordError}</p>}
             </div>
-            <button type="submit">{action === "login" ? "Log In" : "Sign Up"}</button>
-            <BottomText />
+            {!isLoading && (
+                <button type="submit">{action === "login" ? "Log In" : "Sign Up"}</button>
+            )}
+
+            {isLoading && (
+                <button type="submit" className={styles["loading-btn"]}>
+                    {action === "login" ? "Loging In" : "Signing Up"} <Spinner />
+                </button>
+            )}
+            {!isLoading && <BottomText />}
         </form>
     );
 };
--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -1,11 +1,36 @@
 import AuthForm from "../../../components/Auth/AuthForm";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebase/config";
+import { useRouter } from "next/router";
+import { Alert } from "@mui/material";
+import { useState } from "react";
 
 const SignUp = () => {
-    const authHandler = () => {
-        console.log("auth");
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+
+    const authHandler = async (email: string, password: string) => {
+        try {
+            setIsLoading(true);
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            const user = res.user;
+            if (user) {
+                router.push("/");
+            }
+            setIsLoading(false);
+        } catch (error) {
+            const errorMessage = (error as Error).message;
+            setError(errorMessage);
+        }
     };
 
-    return <AuthForm onAuth={authHandler} action={"signup"} />;
+    return (
+        <>
+            {error && <Alert severity="error">{error}</Alert>}
+            <AuthForm onAuth={authHandler} action={"signup"} isLoading={isLoading} />
+        </>
+    );
 };
 
 export default SignUp;
