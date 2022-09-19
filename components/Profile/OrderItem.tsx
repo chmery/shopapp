5c5f207aa7dbf0394845ccb5cd18a91b6403fce9
chmery <karektv@gmail.com>
2022-09-19 15:35:34 +0200

    Add styles for orders history component

---
+++ components/Profile/OrderItem.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import styles from "./OrderItem.module.css";
+import Image from "next/future/image";
+
+const OrderItem = ({ orderData }: { orderData: OrderData }) => {
+    return (
+        <div className={styles.order}>
+            <div className={styles["date-title"]}>
+                <span>{orderData.orderDate}</span>
+                <span>${orderData.totalPrice}</span>
+            </div>
+            <div className={styles["products-images"]}>
+                {orderData.orderedItems.map((item) => (
+                    <div className={styles["product-image"]}>
+                        <Image src={item.image} alt={item.title} fill />
+                    </div>
+                ))}
+            </div>
+            <Link className={styles["details-link"]} href="/order-details">
+                See Order Details
+            </Link>
+        </div>
+    );
+};
+
+export default OrderItem;
--- components/Profile/OrdersHistory.tsx
+++ components/Profile/OrdersHistory.tsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../store/auth-context";
 import styles from "./OrdersHistory.module.css";
 import { collection, query, where, getDocs, DocumentData } from "firebase/firestore";
 import { db } from "../../firebase/config";
+import OrderItem from "./OrderItem";
 
 const OrdersHistory = () => {
     const { userId } = useContext(AuthContext) as AuthContext;
@@ -21,19 +22,15 @@ const OrdersHistory = () => {
 
             setOrders(orders);
         };
-
         fetchOrders();
     }, []);
 
+    const OrdersList = () => orders!.map((order: OrderData) => <OrderItem orderData={order} />);
+
     return (
         <>
-            <h3>Orders history</h3>
-            {orders &&
-                orders.map((order: OrderData) => (
-                    <p>
-                        {order.orderDate} {order.totalPrice}
-                    </p>
-                ))}
+            <h3 className={styles["orders-history"]}>Orders history</h3>
+            {orders && <OrdersList />}
         </>
     );
 };
